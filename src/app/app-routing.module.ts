832cf6b071a13031bb35dfae5b21c4672dfe72d5
FABRE Mathis <f18012254@v-pp-47-029.salsa.univ-amu.fr>
2022-03-17 16:50:11 +0100

    forum site web angular php

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import {CoursComponent} from "./cours/cours.component";
+import {TopicsComponent} from "./topics/topics.component";
+import {PostsComponent} from "./posts/posts.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'cours', component: CoursComponent},
+  {path: 'topics/:id', component: TopicsComponent},
+  {path: 'posts/:id', component: PostsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { CoursComponent } from './cours/cours.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { TopicsComponent } from './topics/topics.component';
+import { PostsComponent } from './posts/posts.component';
+import { BreadcrumsComponent } from './breadcrums/breadcrums.component';
+import { CreateTopicComponent } from './create-topic/create-topic.component';
+import { CreateTopicDialogComponent } from './create-topic-dialog/create-topic-dialog.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    CoursComponent,
+    TopicsComponent,
+    PostsComponent,
+    BreadcrumsComponent,
+    CreateTopicComponent,
+    CreateTopicDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
